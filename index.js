2f8106728deae662cf615c166cee58e351d817af
Alok Kumar <alok61.bd@gmail.com>
2021-10-31 06:00:54 +0600

    crud operation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, Db } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
 const cors=require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,6 +21,7 @@ async function run(){
 
         const database=client.db("Tour-thirsty");
         const packageCollection=database.collection("package");
+        const bookingCollection=database.collection('booking');
          
         app.get('/packages',async(req,res)=>{
           const cursor=packageCollection.find({});
@@ -27,10 +29,42 @@ async function run(){
           res.send(packages)
         })    
       app.get('/packages/:id',async(req,res)=>{
-        const id=req.params.id;
-        console.log(id);
+        // console.log(req.params.id)
+        const packageId=req.params.id;
+        const query={_id:ObjectId(packageId)}
+        const result=await packageCollection.findOne(query);
+        console.log("package get",result)
+        res.json(result)
+      })
+      app.get('/booking',async(req,res)=>{
+        const coursor=bookingCollection.find({})
+        const result=await coursor.toArray();
+        res.send(result);
       })
 
+      app.post('/booking',async( req,res)=>{
+        const bookingInfo=req.body;
+        const result=await bookingCollection.insertOne(bookingInfo);
+        console.log("added successful",result)  
+        // console.log(bookingInfo)
+        res.json(result);
+       })
+      app.post('/packages',async( req,res)=>{
+        const newpackage=req.body;
+        const result=await packageCollection.insertOne(newpackage);
+        console.log("added successful",result)  
+        // console.log(bookingInfo)
+        res.json(result);
+       })
+      
+       app.delete('/packages/:id',async(req,res)=>{
+        const id=req.params.id;
+        console.log(id);
+        const query={_id:ObjectId(id)}
+        const result=await packageCollection.deleteOne(query);
+        console.log("deleting user with id",result);
+        res.json(result);
+     })
     } 
     finally{
         // await client.close();
